39597c7150e48f47578199ade511e51885b29314
mholwill <m_holwill@hotmail.co.uk>
2020-04-06 19:41:41 +0100

    Server DONE

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 
 MongoClient.connect('mongodb://localhost:27017')
   .then(client => {
-    const db = client.db('hotel');
-    const bookingsCollection = db.collection('bookings');
-    const bookingsRouter = createRouter(bookingsCollection);
-    app.use('/api/bookings', bookingsRouter);
+    const db = client.db('codeclanercise');
+    const exercisersCollection = db.collection('exercisers');
+    const exercisersRouter = createRouter(exercisersCollection);
+    app.use('/api/exercisers', exercisersRouter);
   })
   .catch(err => console.error(err));
 app.listen(3000, function() {
